a5e8888f28a8c57e37620e4761b8c428bb3a74b0
Ilya Myaksihev <ilyamyakishev96@gmail.com>
2022-09-04 16:49:34 +0300

    My learn

---
+++ lists.py
@@ -0,0 +1,12 @@
+from tkinter import N
+
+
+num_list = [3, 5, 7, 9, 10.5]
+print(num_list)
+num_list.append('Python')
+print(len(num_list))
+print(num_list[0])
+print(num_list[-1])
+print(num_list[2:5])
+num_list.remove('Python')
+print(num_list)
