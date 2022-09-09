dea66b97c70fa25fcb22ae32ea707dac50580798
Ilya Myaksihev <ilyamyakishev96@gmail.com>
2022-09-09 19:21:49 +0300

    Using getattr

--- bot.py
+++ bot.py
@@ -7,13 +7,11 @@ import ephem
 logging.basicConfig(filename='bot.log', level=logging.INFO)
 
 today = datetime.now()
-planets = {'Mars': ephem.Mars, 'Venus': ephem.Venus, 'Saturn': ephem.Saturn,'Jupiter': ephem.Jupiter,
-               'Neptune': ephem.Neptune, 'Uranus': ephem.Uranus, 'Mercury': ephem.Mercury}
 
 def greet_user(update, context): 
     print('/start just used')
     update.message.reply_text("Hello! You just run my first learn bot! Try it and enjoy!\nYou can use /planet command. Type /planet and planet what you want. For example /planet Mars")
-    # print(update)
+    #print(update)
 
 def talk_to_me(update, context):
     text = update.message.text
@@ -25,16 +23,13 @@ def planet_func(update, context):
     get_planet = update.message.text.split()
     planet = get_planet[1].capitalize()
     print("User choose", planet)
-    planet_for_cons = planets.get(planet)
-    if planet_for_cons != None:
-        current_constellation = ephem.constellation(planets[planet](today))[1]
-        update.message.reply_text(f"Corrent constellation of {planet} is {current_constellation}")
-    else: 
-        update.message.reply_text("Please enter a correct planet")
+    if planet:
+        try: 
+            current_constellation = ephem.constellation(getattr(ephem, planet)(today))[1]
+            update.message.reply_text(f"Corrent constellation of {planet} is {current_constellation}")
+        except AttributeError: 
+            update.message.reply_text("Please enter a correct planet")
 
-    
-
-    pass
 
 def main(): 
     mybot = Updater(settings.API_KEY, use_context=True)
